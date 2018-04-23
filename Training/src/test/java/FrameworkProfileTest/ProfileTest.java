0d00905570b498b3825f9328a435f9a1ad7f34a8
Vincent <vleung1@gmail.com>
2018-04-23 13:14:20 -0400

    Build profile and POM changes

---
+++ Training/src/test/java/FrameworkProfileTest/ProfileTest.java
@@ -0,0 +1,13 @@
+package FrameworkProfileTest;
+
+import java.net.MalformedURLException;
+
+import org.testng.annotations.Test;
+
+public class ProfileTest {
+
+	@Test
+	public void testing() throws MalformedURLException {
+		System.out.println("ok");
+	}
+}
