3c6a91880f08333cd5dc39de053b8e9b3a339e7c
Vincent <vleung1@gmail.com>
2018-04-24 20:24:03 -0400

    First commit, uploading files

---
+++ Log4jExercises/src/beta/Demo2.java
@@ -0,0 +1,20 @@
+package beta;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Demo2 {
+	
+	//this creates a log object to use in code below
+	//initializes it with the name of the class, gotten by using Demo.class.getName() because cannot just pass the name in
+	private static Logger log = LogManager.getLogger(Demo2.class.getName());
+
+	public static void main(String[] args) {
+
+		log.debug("This is a debug message");
+		log.info("This is an info message");
+		log.error("This is a error message");
+		log.fatal("This is a fatal message");
+	}
+
+}
